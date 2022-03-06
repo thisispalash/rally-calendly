01f800a55d32620eced1a2367307f8a771946141
Palash A <palash.agarwal.96@gmail.com>
2022-03-06 17:27:31 +0530

    init

---
+++ src/rally.js
@@ -0,0 +1,35 @@
+
+
+
+class RallyClient {
+  
+  access_token = undefined
+  refresh_token = undefined
+  token_type = undefined
+  expiry = undefined
+
+  constructor(username, password, base_url, v1_url, callback_url) {
+
+  }
+
+  setAuth(data) {
+
+  }
+
+  register() {
+
+  }
+
+  refresh() {
+
+  }
+
+  authenticate() {
+
+  }
+
+  balance(token) {
+
+  }
+
+}
